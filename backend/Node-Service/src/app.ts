e83b22f903b63658fb16b135f7c7870b1873a085
Kayz <kibrahim17@hotmail.com>
2023-12-15 05:59:32 +1100

    Node-Service v1

---
+++ backend/Node-Service/src/app.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response, NextFunction } from "express";
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
