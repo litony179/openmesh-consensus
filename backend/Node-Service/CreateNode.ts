d138e4f04905afca2578befa5653a838ac934506
Kayz <kibrahim17@hotmail.com>
2023-12-14 11:36:10 +1100

    added createnode.ts

--- backend/Node-Service/CreateNode.ts
+++ backend/Node-Service/CreateNode.ts
@@ -1,91 +1,104 @@
+import { v4 as uuidv4 } from "uuid";
+
 class NodeClass {
-    NodeHash: String = "";
-    UserId: String = "";
-    Public_key: String = "";
-    DataMajor: String = "";
-    CreateDate: String = "";
-    ConnectionType: Boolean = false;
+  NodeHash: String = "";
+  UserId: String = "";
+  Public_key: String = "";
+  DataMajor: String = "";
+  CreateDate: String = "";
+  ConnectionType: String = "";
 
-    describeNode(){
-        console.log("NodeHash: " + this.NodeHash);
-        console.log("UserId: " + this.UserId);
-        console.log("Public_key: " + this.Public_key);
-        console.log("DataMajor: " + this.DataMajor);
-        console.log("CreateDate: " + this.CreateDate);
-        console.log("ConnectionType: " + this.ConnectionType);
+  describeNode() {
+    console.log(node);
+  }
 
-        console.log(node);
-    }
+  generateNodeHash() {
+    this.NodeHash = uuidv4();
+    return this.NodeHash;
+  }
 
-    generateNodeHash(){
-        // generate random hash
-    }
+  generatePublic_key() {
+    this.Public_key = uuidv4();
+    return this.Public_key;
+  }
+
+  DateCreation() {
+    let currentDate: Date = new Date();
+    let year: number = currentDate.getFullYear();
+    let month: number = currentDate.getMonth(); // 0-based (January is 0, February is 1, etc.)
+    let day: number = currentDate.getDate();
+    let hours: number = currentDate.getHours();
+    let minutes: number = currentDate.getMinutes();
+    let seconds: number = currentDate.getSeconds();
+    this.CreateDate = `${day}-${
+      month + 1
+    }-${year} ${hours}:${minutes}:${seconds}`;
+
+    return this.CreateDate;
+  }
 }
 
-interface NodeBuilderInt{
-    SetNodeHash(NodeHash: String): void;
-    SetUserID(UserId: String): void;
-    SetPublickey(Public_key: String): void;
-    SetDataMajor(DataMajor: String): void;
-    SetCreateDate(CreateDate: String): void;
-    SetConnectionType(ConnectionType: boolean): void;
+interface NodeBuilderInt {
+  SetNodeHash(): void;
+  SetUserID(UserId: String): void;
+  SetPublickey(): void;
+  SetDataMajor(DataMajor: String): void;
+  SetCreateDate(): void;
+  SetConnectionType(ConnectionType: String): void;
 
-    build(): NodeClass;
+  build(): NodeClass;
 }
 
-class NodeBuilder implements NodeBuilderInt{
-    private node: NodeClass;
+class NodeBuilder implements NodeBuilderInt {
+  private node: NodeClass;
 
-    constructor(){
-        this.node = new NodeClass;
-    }
+  constructor() {
+    this.node = new NodeClass();
+  }
 
-    SetNodeHash(NodeHash: String): void {
-        this.node.NodeHash = NodeHash;   
-    }
+  SetNodeHash(): void {
+    this.node.generateNodeHash();
+  }
 
-    SetUserID(UserId: String): void{
-        this.node.UserId = UserId;
-    }
+  SetUserID(UserId: String): void {
+    this.node.UserId = UserId;
+  }
 
-    SetPublickey(Public_key: String): void{
-        this.node.Public_key = Public_key;
-    }
-    SetDataMajor(DataMajor: String): void{
-        this.node.DataMajor = DataMajor;
-    }
+  SetPublickey(): void {
+    this.node.generatePublic_key();
+  }
+  SetDataMajor(DataMajor: String): void {
+    this.node.DataMajor = DataMajor;
+  }
 
-    SetCreateDate(CreateDate: String): void{
-        this.node.CreateDate = CreateDate;
-    }
+  SetCreateDate(): void {
+    this.node.DateCreation();
+  }
 
-    SetConnectionType(ConnectionType: boolean): void{
-        this.node.ConnectionType = ConnectionType;
-    }
+  SetConnectionType(ConnectionType: String): void {
+    this.node.ConnectionType = ConnectionType;
+  }
 
-
-    build(): NodeClass{
-        return this.node;
-    }
+  build(): NodeClass {
+    return this.node;
+  }
 }
 
 let NodeBuilderInt = new NodeBuilder();
 // Data inside is hash example
 // Randomly generated
-NodeBuilderInt.SetNodeHash("AX3");
+NodeBuilderInt.SetNodeHash();
 // RawData given from Uploader later
 // given from auth
 NodeBuilderInt.SetUserID("001");
 //randomly generated and given
-NodeBuilderInt.SetPublickey("XXA#@25jkXD");
+NodeBuilderInt.SetPublickey();
 //Set by user
 NodeBuilderInt.SetDataMajor("Health");
 //Instantly generated
-NodeBuilderInt.SetCreateDate("02/03/2023");
+NodeBuilderInt.SetCreateDate();
 //Selected by user
-NodeBuilderInt.SetConnectionType(true);
-
+NodeBuilderInt.SetConnectionType("Restful");
 
 let node = NodeBuilderInt.build();
 node.describeNode();
-
---
+++ backend/Node-Service/test.ts
@@ -0,0 +1,3 @@
+import * as bip32 from 'bip32';
+
+console.log(bip32.BIP32Factory);
