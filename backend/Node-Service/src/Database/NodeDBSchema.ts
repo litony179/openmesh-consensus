7e6df009f10951c7e0b471ee8ed289d44d9c45f3
Kayz <kibrahim17@hotmail.com>
2023-12-16 02:28:30 +1100

    Mongo Update

---
+++ backend/Node-Service/src/Database/NodeDBSchema.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface NodeDBInt extends Document {
+  NodeHash: String;
+  UserId: String;
+  Public_key: String;
+  DataMajor: String;
+  CreateDate: String;
+  ConnectionType: String;
+}
+
+const NodedbSchema: Schema = new Schema({
+  NodeHash: { type: String, required: true },
+  UserId: { type: String, required: true },
+  Public_key: { type: String, required: true },
+  DataMajor: { type: String, required: true },
+  CreateDate: { type: String, required: true },
+  ConnectionType: { type: String, required: true },
+});
+
+export default mongoose.model<NodeDBInt>("Example", NodedbSchema);
