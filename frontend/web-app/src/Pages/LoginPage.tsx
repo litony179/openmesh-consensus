a142f9864566b14f74e5a4a4b6229ab19eed7ec3
Christopher <z5373462@ad.unsw.edu.au>
2023-12-16 15:34:00 +1100

    aded images

--- frontend/web-app/src/Pages/LoginPage.tsx
+++ frontend/web-app/src/Pages/LoginPage.tsx
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { useState, FormEvent } from 'react';
+import '../Styles/App.scss';
+import '../Styles/Login.scss';
 
-export const LoginPage = () => {
-  return (<>LoginPage</>);
+interface LoginState {
+  email: string;
+  password: string;
 }
+
+export const LoginPage: React.FC = () => {
+  const [loginData, setLoginData] = useState<LoginState>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // Here you would handle the login submission, e.g., by sending the data to an API
+    console.log('Login data:', loginData);
+  };
+
+  return (
+    <>
+      <div className='d-flex'>
+        <img src='../Components/login-register-img.jpeg' alt='logo' className='w-50'/>
+        <form className="login-form w-70" onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={loginData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={loginData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="login-btn">Login</button>
+          <div className="footer-links">
+            <a href="/forgot-password">Forgot Password?</a>
+            <span>No account? <a href="/register">Register</a></span>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
--- frontend/web-app/src/Pages/SignupPage.tsx
+++ frontend/web-app/src/Pages/SignupPage.tsx
@@ -1,5 +1,97 @@
-import React from "react";
+import React, { useState, FormEvent } from 'react';
+import '../Styles/Signup.scss';
 
-export const SignupPage = () => {
-  return (<>SignupPage</>);
+interface FormState {
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobileNumber: string;
+  password: string;
 }
+
+export const SignupPage: React.FC = () => {
+  const [formData, setFormData] = useState<FormState>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobileNumber: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // Here you would handle the form submission, e.g., by sending the data to an API
+    console.log('Form data:', formData);
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First name"
+          value={formData.firstName}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last name"
+          value={formData.lastName}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="tel"
+          name="mobileNumber"
+          placeholder="Mobile number"
+          value={formData.mobileNumber}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="password"
+          name="password"
+          placeholder="Create a password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label>
+          <input type="checkbox" required />
+          I agree to the Terms and Privacy Policy
+        </label>
+      </div>
+      <button type="submit" className="submit-btn">Create account</button>
+      <div className="login-link">
+        Already have an account? <a href="/login">Log in</a>
+      </div>
+    </form>
+  );
+};
