8a7f82b6e1b21825c826f3627047480ef0fd7798
kkevin811015 <kkevin811015@gmail.com>
2023-12-17 17:46:30 +1100

    upload to S3 process are written

--- backend/uploader-service/src/controllers/upload-file-controller.ts
+++ backend/uploader-service/src/controllers/upload-file-controller.ts
@@ -8,6 +8,8 @@ import { logEvents } from "../middleware/log-events";
 import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { awsS3Client } from "../services/s3";
+import { S3Config, awsConfig } from "../config/aws-config";
+import { UploadFile } from "../models/upload-file";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -16,7 +18,7 @@ const upload = multer({ storage });
 const uploadFileHandler = asyncHandler(async (req: Request, res: Response) => {
   /*
    * Step 1: Get request information and do validation
-   * step 2:
+   * step 2: Save 'request_file' to S3 & Save file's 'metadata' to MongoDB
    */
 
   // retrieve file from request body
@@ -45,12 +47,37 @@ const uploadFileHandler = asyncHandler(async (req: Request, res: Response) => {
   console.log("This is the request body: ", req.body);
   console.log("This is the request image/file", req.file);
 
+  // Remember 'requestFile'
   const fileMetadata = requestFileMetadata.data;
 
   console.log("This is the file metadata: ", fileMetadata);
 
+  // Present: File & MetaData all inspected
+  // So, this time, "ACTIVATE S3!"
+
+  // Create Bucket ( Creating client & Checking bucket already applied)
+  const s3Bucket = await awsS3Client.createS3Bucket(S3Config, "bucksbucks");
+
+  if (s3Bucket === null) {
+    // if (s3Bucket!) {
+
+    const uploadingFile = awsS3Client.uploadFile(
+      s3Bucket,
+      requestFile,
+      { bucketName: s3Bucket, key: "key", contentType: "contentType" });
+
+    const uploadingMetaData = UploadFile.build(fileMetadata).save(this);
+
+    const uploadingProcess = await Promise.all([
+      uploadingFile, uploadingMetaData
+    ]);
+
+  } else {
+    res.status(500).send("Server Error: Bucket creation failed!")
+  }
+
   res.status(200).send("Hello");
-  // // post -> check -> encrypt -> save
+
 });
 
 export { uploadFileHandler };
