7e2a94e1d1d4067d80bcb3316c5b5a575c9ea3a4
Kayz <kibrahim17@hotmail.com>
2023-12-18 12:13:18 +1100

    CreateNode-controller finished with saving to mongo finished, Awaiting testing

--- backend/Node-Service/src/controllers/CreateNode-controller.ts
+++ backend/Node-Service/src/controllers/CreateNode-controller.ts
@@ -2,22 +2,21 @@
 import express, { Request, Response } from "express";
 import { NodeClass } from "../Models/NodeDefined";
 import { decrypttoken } from "../controllers/DecryptJwt-controller";
+import { NodeModel } from '../Schema/NodeDBSchema';
 
 const router = express.Router();
 
-router.post("/create", (req: Request, res: Response) => {
-    // Grabs data from the request body and find the current UserId of the user logged in 
-  const { DataMajor, ConnectionType } = req.body;
-  const UserId = decrypttoken(res.header("Authorization"));
-
-  // Creates a NewNode based off the class and given data above
-  const NewNode = new NodeClass(UserId, DataMajor, ConnectionType);
-
+router.post("/create", async (req: Request, res: Response) => {
   try {
+    // Grabs data from the request body and find the current UserId of the user logged in
+    const { DataMajor, ConnectionType } = req.body;
+    const UserId = decrypttoken(res.header("Authorization"));
+    // Creates a NewNode based off the class and given data above
+    const NewNode = new NodeClass(UserId, DataMajor, ConnectionType);
     // Save the new node to MongoDB using the NodeModel
     const savedNode = await NodeModel.create(NewNode);
     console.log("Object saved to MongoDB:", savedNode);
-    res.status(200).json(savedNode);
+    res.json(savedNode);
   } catch (error) {
     console.error("Error saving object:", error);
     res.status(500).json({ error: "Internal Server Error" });
