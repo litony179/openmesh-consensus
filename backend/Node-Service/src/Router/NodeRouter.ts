0126a8b480e084eb6d9a12f009cc2cb4cb431675
Kayz <kibrahim17@hotmail.com>
2023-12-21 00:32:39 +1100

    Newest version of backend

--- backend/Node-Service/src/Router/NodeRouter.ts
+++ backend/Node-Service/src/Router/NodeRouter.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import { CreateNode } from "../controllers/CreateNode-controller";
 import { DeleteNode } from "../controllers/DeleteNode-controller";
 import { FetchNode } from "../controllers/FetchNode-controller";
-import { verifyJWT } from "../middleware/verify-jwt";
 
 const router = express.Router();
 
--- backend/Node-Service/src/controllers/FetchNode-controller.ts
+++ backend/Node-Service/src/controllers/FetchNode-controller.ts
@@ -1,39 +1,15 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import { NodeModel } from "../Schema/NodeDBSchema";
-import { decryptToken } from "./DecryptJwt-controller";
 
 /**
  @precondition nodeId is in the request parameter
  **/
 const FetchNode = asyncHandler(async (req: Request, res: Response) => {
   const nodeId = req.params.nodeId;
-  const userId = decryptToken(req.headers.cookie);
 
   const node = await NodeModel.findById(nodeId);
   res.json(node);
 }); 
-
-// router.post("Fetch Node", async (req: Request, res: Response) => {
-//   try {
-//     const nodeId = req.params.nodeId;
-//     const userId = decrypttoken(res.header("Authorization"));
-
-//     const node = await NodeModel.findById(nodeId);
-
-//     if(!node){
-//         return res.status(400).json({error: "Node not found"});
-//     }
-
-//     if(node.UserId != String(userId)){
-//         return res.status(403).json({error: "Unauthorized"})
-//     }
-
-//     res.json(node);
-//   }catch (error){
-//     console.error(error);
-//     res.status(500).json({error: "Internal Server error"});
-//   }
-// });
 
 export { FetchNode };
